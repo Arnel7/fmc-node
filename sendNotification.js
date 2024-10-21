d75887bbe5e2e37c0b6d5003acf17511a652a198
LIBERA LAWSON <62177552+Arnel7@users.noreply.github.com>
2024-10-21 20:42:17 +0200

    Update sendNotification.js

--- sendNotification.js
+++ sendNotification.js
@@ -23,5 +23,5 @@ function sendNotification(token, title, body) {
 
 // Exemple d'utilisation
 const token =
-  "fYtEkIKKgrLN8erMrr7Fzf:APA91bHmk3SdnC1oo6kac9frtDO1qydn758M1qp2bNcUEOKrwl6q5Qv7A3195PVm05VZSBkCmEK5Db-AnAdglNIBznvcZ6c826pnDBh8C-jXACIV5RYX0shVEj5000gWwGYGyHoKD45F"; // Remplacez par le token du client
+  ""; // Remplacez par le token du client
 sendNotification(token, "Coding TEAM", "Exemple de notification ");
